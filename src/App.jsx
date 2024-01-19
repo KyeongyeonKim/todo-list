1aa16dd529141ce628c8fd01bca1a6a717c2eabc
KY <rudyeon@naver.com>
2024-01-19 14:22:40 +0900

    repository 초기 파일 생성

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <div>My Todo List</div>
+      <div>
+        제목 <input type="text" />
+      </div>
+      <div>
+        내용 <input type="text" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
