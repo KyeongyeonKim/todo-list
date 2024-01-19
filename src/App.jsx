f3b7a282bad3e9727a036fbc9b38c4d4dd3031b6
KY <rudyeon@naver.com>
2024-01-19 21:58:22 +0900

    [Feat] working, done deleteBtnHandler 삭제버튼 구현

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,17 @@ function App() {
       isDone: false,
     },
   ]);
+  const [doneList, setDoneList] = useState([
+    {
+      id: 0,
+      title: "웹종합 공부하기",
+      content: "웹종합 2회 완강하기",
+      isDone: true,
+    },
+  ]);
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+  const [isDone, setIsDone] = useState("false");
 
   const titleHandler = (event) => {
     setTitle(event.target.value);
@@ -19,7 +28,7 @@ function App() {
   const contentHandler = (event) => {
     setContent(event.target.value);
   };
-  const addListHandler = () => {
+  const addBtnHandler = () => {
     const newList = {
       id: todoList.length + 1,
       title,
@@ -29,11 +38,22 @@ function App() {
     setTodoList([...todoList, newList]);
   };
 
-  const deleteListHandler = (id) => {
+  const deleteBtnHandler = (id, isDone) => {
     // filter를 사용해서 조건에 해당하는 것만 출력! 클릭한것의 id와 같지 않으면 출력!
-    const newList = todoList.filter((list) => list.id !== id);
-    setTodoList(newList);
+    if (isDone === false) {
+      const newList = todoList.filter((list) => list.id !== id);
+      setTodoList(newList);
+    } else {
+      const newList = doneList.filter((list) => list.id !== id);
+      setDoneList(newList);
+    }
   };
+
+  // 완료/취소 버튼
+  // 버튼을 누르면 isdone의 값을 바꿔준다.
+  // const onChangeHandler =(id)=>{
+
+  // }
 
   return (
     <div>
@@ -42,7 +62,7 @@ function App() {
         제목 <input type="text" value={title} onChange={titleHandler} />
         내용 <input type="text" value={content} onChange={contentHandler} />
       </div>
-      <button onClick={addListHandler}>추가하기</button>
+      <button onClick={addBtnHandler}>추가하기</button>
       <div>Working...</div>
       <div className="app-style">
         {todoList.map((list) => {
@@ -50,7 +70,7 @@ function App() {
             <div key={list.id} className="component-style">
               <div>{list.title}</div>
               <div>{list.content}</div>
-              <button onClick={() => deleteListHandler(list.id)}>
+              <button onClick={() => deleteBtnHandler(list.id, list.isDone)}>
                 삭제하기
               </button>
               <button>완료</button>
@@ -60,11 +80,19 @@ function App() {
       </div>
 
       <div>Done...!</div>
-      <div>
-        <div>자바스크립트 공부하기</div>
-        <div>JS 기초를 공부해봅시다.</div>
-        <button>삭제</button>
-        <button>취소</button>
+      <div className="app-style">
+        {doneList.map((list) => {
+          return (
+            <div key={list.id} className="component-style">
+              <div>{list.title}</div>
+              <div>{list.content}</div>
+              <button onClick={() => deleteBtnHandler(list.id, list.isDone)}>
+                삭제하기
+              </button>
+              <button>취소</button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
